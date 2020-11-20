39fef0907aa974a9daf36aa8fb12d02431ee9497
Ivan.v <1v4nvl@gmail.com>
2020-11-20 12:09:55 +0200

    new mc version

--- dist/motorcortex-typewriting.cjs.js
+++ dist/motorcortex-typewriting.cjs.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
-var MC = _interopDefault(require('@kissmybutton/motorcortex'));
+var MotorCortex = _interopDefault(require('@kissmybutton/motorcortex'));
 
 function _classCallCheck(instance, Constructor) {
   if (!(instance instanceof Constructor)) {
@@ -223,7 +223,14 @@ function _createSuper(Derived) {
   };
 }
 /*
- * anime.js v3.1.0
+ * anime.js v3.1.4
+ * (c) 2020 Julian Garnier
+ * Released under the MIT license
+ * animejs.com
+ */
+
+/*
+ * anime.js v3.1.2
  * (c) 2020 Julian Garnier
  * Released under the MIT license
  * animejs.com
@@ -883,7 +890,10 @@ function createNewInstance(params) {
 
 
 function anime(params) {
-  if (params === void 0) params = {};
+  if (params === void 0) {
+    params = {};
+  }
+
   var children,
       childrenLength = 0;
   var resolve = null;
@@ -1153,23 +1163,25 @@ function getParentSvg(pathEl, svgData) {
   };
 }
 
-function getPath(path, percent) {
-  var pathEl = is.str(path) ? selectString(path)[0] : path;
-  var p = percent || 100;
-  return function (property) {
-    return {
-      property: property,
-      el: pathEl,
-      svg: getParentSvg(pathEl),
-      totalLength: getTotalLength(pathEl) * (p / 100)
-    };
+function getPath(path) {
+  return {
+    el: path,
+    svg: getParentSvg(path),
+    totalLength: getTotalLength(path),
+    deltaCorrections: {
+      x: 4,
+      y: 5
+    }
   };
 }
 
 function getPathProgress(path, progress, isPathTargetInsideSVG) {
   function point(offset) {
     if (offset === void 0) offset = 0;
-    var l = progress + offset >= 1 ? progress + offset : 0;
+
+    var _progress = progress * path.totalLength;
+
+    var l = _progress + offset >= 1 ? _progress + offset : 0;
     return path.el.getPointAtLength(l);
   }
 
@@ -1179,17 +1191,11 @@ function getPathProgress(path, progress, isPathTargetInsideSVG) {
   var p1 = point(+1);
   var scaleX = isPathTargetInsideSVG ? 1 : svg.w / svg.vW;
   var scaleY = isPathTargetInsideSVG ? 1 : svg.h / svg.vH;
-
-  switch (path.property) {
-    case 'x':
-      return (p.x - svg.x) * scaleX;
-
-    case 'y':
-      return (p.y - svg.y) * scaleY;
-
-    case 'angle':
-      return Math.atan2(p1.y - p0.y, p1.x - p0.x) * 180 / Math.PI;
-  }
+  return {
+    x: (p.x - svg.x) * scaleX,
+    y: (p.y - svg.y) * scaleY,
+    angle: Math.atan2(p1.y - p0.y, p1.x - p0.x) * 180 / Math.PI
+  };
 }
 
 anime.version = '3.1.0';
@@ -1199,6 +1205,7 @@ anime.convertPx = convertPxToUnit;
 anime.penner = penner;
 anime.path = getPath;
 anime.getPathProgress = getPathProgress;
+var anime_es = anime;
 var transform = ["translateX", "translateY", "translateZ", "rotate", "rotateX", "rotateY", "rotateZ", "scale", "scaleX", "scaleY", "scaleZ", "skewX", "skewY", "perspective"];
 var compositeAttributes = {
   transform: transform
@@ -1244,8 +1251,8 @@ function getMatrix2D(win, element) {
 
 var Anime =
 /*#__PURE__*/
-function (_MC$Effect) {
-  _inherits$1(Anime, _MC$Effect);
+function (_MotorCortex$Effect) {
+  _inherits$1(Anime, _MotorCortex$Effect);
 
   var _super = _createSuper(Anime);
 
@@ -1274,7 +1281,7 @@ function (_MC$Effect) {
         options[this.attributeKey] = [this.getInitialValue(), this.targetValue];
       }
 
-      this.target = anime(_objectSpread2({
+      this.target = anime_es(_objectSpread2({
         autoplay: false,
         duration: this.props.duration,
         easing: "linear",
@@ -1293,14 +1300,14 @@ function (_MC$Effect) {
           if (Object.prototype.hasOwnProperty.call(currentTransform, transform[i])) {
             obj[transform[i]] = currentTransform[transform[i]];
           } else {
-            obj[transform[i]] = anime.get(this.element, transform[i]);
+            obj[transform[i]] = anime_es.get(this.element, transform[i]);
           }
         }
 
         return obj;
       }
 
-      return anime.get(this.element, this.attributeKey);
+      return anime_es.get(this.element, this.attributeKey);
     }
     /**
      * @param {number} f
@@ -1314,7 +1321,52 @@ function (_MC$Effect) {
   }]);
 
   return Anime;
-}(MC.Effect);
+}(MotorCortex.Effect);
+/**
+ * Takes as attributes:
+ * {
+ *  animatedAttrs: {
+ *      positionOn: {
+ *          pathElement: "selector of the path element"
+ *      }
+ *  }
+ * }
+ }
+**/
+
+
+var MotionPath =
+/*#__PURE__*/
+function (_MotorCortex$Effect) {
+  _inherits$1(MotionPath, _MotorCortex$Effect);
+
+  var _super = _createSuper(MotionPath);
+
+  function MotionPath() {
+    _classCallCheck$1(this, MotionPath);
+
+    return _super.apply(this, arguments);
+  }
+
+  _createClass$1(MotionPath, [{
+    key: "onGetContext",
+    value: function onGetContext() {
+      var svgEl = this.context.getElements(this.targetValue.pathElement)[0];
+      this.path = anime_es.path(svgEl);
+      this.isPathTargetInsideSVG = this.element instanceof SVGElement;
+    }
+  }, {
+    key: "onProgress",
+    value: function onProgress(f) {
+      var position = anime_es.getPathProgress(this.path, f, this.isPathTargetInsideSVG); // console.log(position);
+
+      var toSet = "\n            translateX(".concat(position.x, "px) \n            translateY(").concat(position.y, "px) \n            rotate(").concat(position.angle, "deg)\n        ");
+      this.element.style.transform = toSet;
+    }
+  }]);
+
+  return MotionPath;
+}(MotorCortex.Effect);
 
 var nu = ["cm", "mm", "in", "px", "pt", "pc", "em", "ex", "ch", "rem", "vw", "vh", "vmin", "vmax", "%"];
 var ru = ["deg", "rad", "grad", "turn"];
@@ -2187,11 +2239,29 @@ var index = {
     attributesValidationRules: {
       animatedAttrs: animatedAttrs
     }
+  }, {
+    exportable: MotionPath,
+    name: "MotionPath",
+    attributesValidationRules: {
+      animatedAttrs: {
+        type: "object",
+        props: {
+          positionOn: {
+            type: "object",
+            props: {
+              pathElement: {
+                type: "string"
+              }
+            }
+          }
+        }
+      }
+    }
   }],
   compositeAttributes: compositeAttributes
 };
 
-var Anime$1 = MC.loadPlugin(index);
+var Anime$1 = MotorCortex.loadPlugin(index);
 
 var TypeWriting =
 /*#__PURE__*/
@@ -2215,7 +2285,7 @@ function (_MotorCortex$HTMLClip) {
         html3 += html;
       }
 
-      var word = new MC.HTMLClip({
+      var word = new MotorCortex.HTMLClip({
         css: this.css,
         html: " <div class=\"textContainer\" >".concat(html3, " </div>"),
         selector: ".onemore"
@@ -2312,7 +2382,7 @@ function (_MotorCortex$HTMLClip) {
   }]);
 
   return TypeWriting;
-}(MC.HTMLClip);
+}(MotorCortex.HTMLClip);
 
 var TypeWriting_1 = TypeWriting;
 
--- dist/motorcortex-typewriting.esm.js
+++ dist/motorcortex-typewriting.esm.js
@@ -1,4 +1,4 @@
-import MC from '@kissmybutton/motorcortex';
+import MotorCortex from '@kissmybutton/motorcortex';
 
 function _classCallCheck(instance, Constructor) {
   if (!(instance instanceof Constructor)) {
@@ -217,7 +217,14 @@ function _createSuper(Derived) {
   };
 }
 /*
- * anime.js v3.1.0
+ * anime.js v3.1.4
+ * (c) 2020 Julian Garnier
+ * Released under the MIT license
+ * animejs.com
+ */
+
+/*
+ * anime.js v3.1.2
  * (c) 2020 Julian Garnier
  * Released under the MIT license
  * animejs.com
@@ -877,7 +884,10 @@ function createNewInstance(params) {
 
 
 function anime(params) {
-  if (params === void 0) params = {};
+  if (params === void 0) {
+    params = {};
+  }
+
   var children,
       childrenLength = 0;
   var resolve = null;
@@ -1147,23 +1157,25 @@ function getParentSvg(pathEl, svgData) {
   };
 }
 
-function getPath(path, percent) {
-  var pathEl = is.str(path) ? selectString(path)[0] : path;
-  var p = percent || 100;
-  return function (property) {
-    return {
-      property: property,
-      el: pathEl,
-      svg: getParentSvg(pathEl),
-      totalLength: getTotalLength(pathEl) * (p / 100)
-    };
+function getPath(path) {
+  return {
+    el: path,
+    svg: getParentSvg(path),
+    totalLength: getTotalLength(path),
+    deltaCorrections: {
+      x: 4,
+      y: 5
+    }
   };
 }
 
 function getPathProgress(path, progress, isPathTargetInsideSVG) {
   function point(offset) {
     if (offset === void 0) offset = 0;
-    var l = progress + offset >= 1 ? progress + offset : 0;
+
+    var _progress = progress * path.totalLength;
+
+    var l = _progress + offset >= 1 ? _progress + offset : 0;
     return path.el.getPointAtLength(l);
   }
 
@@ -1173,17 +1185,11 @@ function getPathProgress(path, progress, isPathTargetInsideSVG) {
   var p1 = point(+1);
   var scaleX = isPathTargetInsideSVG ? 1 : svg.w / svg.vW;
   var scaleY = isPathTargetInsideSVG ? 1 : svg.h / svg.vH;
-
-  switch (path.property) {
-    case 'x':
-      return (p.x - svg.x) * scaleX;
-
-    case 'y':
-      return (p.y - svg.y) * scaleY;
-
-    case 'angle':
-      return Math.atan2(p1.y - p0.y, p1.x - p0.x) * 180 / Math.PI;
-  }
+  return {
+    x: (p.x - svg.x) * scaleX,
+    y: (p.y - svg.y) * scaleY,
+    angle: Math.atan2(p1.y - p0.y, p1.x - p0.x) * 180 / Math.PI
+  };
 }
 
 anime.version = '3.1.0';
@@ -1193,6 +1199,7 @@ anime.convertPx = convertPxToUnit;
 anime.penner = penner;
 anime.path = getPath;
 anime.getPathProgress = getPathProgress;
+var anime_es = anime;
 var transform = ["translateX", "translateY", "translateZ", "rotate", "rotateX", "rotateY", "rotateZ", "scale", "scaleX", "scaleY", "scaleZ", "skewX", "skewY", "perspective"];
 var compositeAttributes = {
   transform: transform
@@ -1238,8 +1245,8 @@ function getMatrix2D(win, element) {
 
 var Anime =
 /*#__PURE__*/
-function (_MC$Effect) {
-  _inherits$1(Anime, _MC$Effect);
+function (_MotorCortex$Effect) {
+  _inherits$1(Anime, _MotorCortex$Effect);
 
   var _super = _createSuper(Anime);
 
@@ -1268,7 +1275,7 @@ function (_MC$Effect) {
         options[this.attributeKey] = [this.getInitialValue(), this.targetValue];
       }
 
-      this.target = anime(_objectSpread2({
+      this.target = anime_es(_objectSpread2({
         autoplay: false,
         duration: this.props.duration,
         easing: "linear",
@@ -1287,14 +1294,14 @@ function (_MC$Effect) {
           if (Object.prototype.hasOwnProperty.call(currentTransform, transform[i])) {
             obj[transform[i]] = currentTransform[transform[i]];
           } else {
-            obj[transform[i]] = anime.get(this.element, transform[i]);
+            obj[transform[i]] = anime_es.get(this.element, transform[i]);
           }
         }
 
         return obj;
       }
 
-      return anime.get(this.element, this.attributeKey);
+      return anime_es.get(this.element, this.attributeKey);
     }
     /**
      * @param {number} f
@@ -1308,7 +1315,52 @@ function (_MC$Effect) {
   }]);
 
   return Anime;
-}(MC.Effect);
+}(MotorCortex.Effect);
+/**
+ * Takes as attributes:
+ * {
+ *  animatedAttrs: {
+ *      positionOn: {
+ *          pathElement: "selector of the path element"
+ *      }
+ *  }
+ * }
+ }
+**/
+
+
+var MotionPath =
+/*#__PURE__*/
+function (_MotorCortex$Effect) {
+  _inherits$1(MotionPath, _MotorCortex$Effect);
+
+  var _super = _createSuper(MotionPath);
+
+  function MotionPath() {
+    _classCallCheck$1(this, MotionPath);
+
+    return _super.apply(this, arguments);
+  }
+
+  _createClass$1(MotionPath, [{
+    key: "onGetContext",
+    value: function onGetContext() {
+      var svgEl = this.context.getElements(this.targetValue.pathElement)[0];
+      this.path = anime_es.path(svgEl);
+      this.isPathTargetInsideSVG = this.element instanceof SVGElement;
+    }
+  }, {
+    key: "onProgress",
+    value: function onProgress(f) {
+      var position = anime_es.getPathProgress(this.path, f, this.isPathTargetInsideSVG); // console.log(position);
+
+      var toSet = "\n            translateX(".concat(position.x, "px) \n            translateY(").concat(position.y, "px) \n            rotate(").concat(position.angle, "deg)\n        ");
+      this.element.style.transform = toSet;
+    }
+  }]);
+
+  return MotionPath;
+}(MotorCortex.Effect);
 
 var nu = ["cm", "mm", "in", "px", "pt", "pc", "em", "ex", "ch", "rem", "vw", "vh", "vmin", "vmax", "%"];
 var ru = ["deg", "rad", "grad", "turn"];
@@ -2181,11 +2233,29 @@ var index = {
     attributesValidationRules: {
       animatedAttrs: animatedAttrs
     }
+  }, {
+    exportable: MotionPath,
+    name: "MotionPath",
+    attributesValidationRules: {
+      animatedAttrs: {
+        type: "object",
+        props: {
+          positionOn: {
+            type: "object",
+            props: {
+              pathElement: {
+                type: "string"
+              }
+            }
+          }
+        }
+      }
+    }
   }],
   compositeAttributes: compositeAttributes
 };
 
-var Anime$1 = MC.loadPlugin(index);
+var Anime$1 = MotorCortex.loadPlugin(index);
 
 var TypeWriting =
 /*#__PURE__*/
@@ -2209,7 +2279,7 @@ function (_MotorCortex$HTMLClip) {
         html3 += html;
       }
 
-      var word = new MC.HTMLClip({
+      var word = new MotorCortex.HTMLClip({
         css: this.css,
         html: " <div class=\"textContainer\" >".concat(html3, " </div>"),
         selector: ".onemore"
@@ -2306,7 +2376,7 @@ function (_MotorCortex$HTMLClip) {
   }]);
 
   return TypeWriting;
-}(MC.HTMLClip);
+}(MotorCortex.HTMLClip);
 
 var TypeWriting_1 = TypeWriting;
 
